fa14765f081dbbc5a9edf83bc1daf730bd1520b5
o-bedford <semeyecolon@gmail.com>
2022-11-06 00:58:01 -0500

    SQLite integration, Really basic deck population,

--- entities/cardsystem/scripts/CardData.gd
+++ entities/cardsystem/scripts/CardData.gd
@@ -19,7 +19,22 @@ var description: String = "Test Description. This is a long one."
 
 var actions: Array = []
 
+var status_effect: String = "Probably don't want this to be a string"
 
-#Loads a JSON into all the fields
-func loadJSON(cardJSON: String) -> bool:
+##Loads a JSON into all the fields
+#func loadJSON(cardJSON: String) -> bool:
+#	return false
+
+func loadDataFromDB(data: Dictionary) -> bool:
+	id = data["ID"]
+#	topic = data["Topic"]
+	SP = data["SP"]
+	quip = data["Blurb"] # Or dialogue. Not sure
+	if data["Dialogue"] != null:
+		description = data["Dialogue"]
+	if data["Action"] != null:
+		actions = data["Action"]
+	if data["Status"] != null:
+		status_effect = data["Status"]
+	
 	return false
--- scenes/Game.gd
+++ scenes/Game.gd
@@ -8,13 +8,29 @@ shall we do a state machine?
 The game is 
 """
 
+const SQLite = preload("res://addons/godot-sqlite/bin/gdsqlite.gdns")
+const db_table = "Cards"
+
+var cardDB:SQLite = SQLite.new()
+
 onready var player1: Player = $Player
 onready var player2: Player = $PlayerAI
 
 
-
 #big boi
 func _ready() -> void:
-	
+	cardDB.path = "res://db/cards"
 	#initializes both decks. 
-	pass
+	populate_deck(player1.deck)
+	populate_deck(player2.deck)
+	for i in player1.deck.deck:
+		print(i.quip)
+
+func populate_deck(deck:Deck) -> void:
+	# Probably add a check for the amount of cards of each topic type
+	cardDB.open_db()
+	cardDB.query("select * from " + db_table + ";")
+	for i in range(0, cardDB.query_result.size()):
+		var newCard = CardData.new()
+		newCard.loadDataFromDB(cardDB.query_result[i])
+		deck.deck.append(newCard)
