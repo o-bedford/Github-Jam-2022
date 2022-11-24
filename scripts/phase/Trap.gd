9484a1393b827d10fa346b164e6dd21c177f31df
o-bedford <semeyecolon@gmail.com>
2022-11-23 22:57:05 -0600

    Fixed a lil bug

--- scripts/phase/Trap.gd
+++ scripts/phase/Trap.gd
@@ -12,13 +12,13 @@ func enter(_msg := {}):
 	trapTimer.start()
 	print("Trap!")
 	opponent = phase_manager.current_unfocused_player
+	opponent.hand.changeState(phase_manager.trapList)
 	if _msg.has("card"):
 		selectedCard = _msg["card"]
 		modifiedCard = selectedCard
 
 func update_phase(delta:float) -> void:
 	# Allow opponent to play trap card
-	opponent.hand.changeState(phase_manager.trapList)
 	if opponent.hand.cardSelected != -1:
 		modifiedCard.SP += opponentCard.SP
 		phase_manager.transition_to("Resolve", {card = modifiedCard})
